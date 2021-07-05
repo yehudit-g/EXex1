7dc704308451ee4c1e20a0e477649af9f9b07fa4
יהודית ליפשיץ <ylifshit@g.jct.ac.il>
2021-07-05 12:43:05 +0300

    mp2-BVH test- build scene

--- tests/renderer/MP2_BVH.java
+++ tests/renderer/MP2_BVH.java
@@ -125,7 +125,7 @@ public class MP2_BVH {
 
     @Test
     public void BVHpic() {
-        Camera camera = new Camera(new Point3D(-200, 0, 1000), new Vector(200, 0, -1000), new Vector(0, 1, 0)) //
+        Camera camera = new Camera(new Point3D(-300, 0, 1000), new Vector(300, 0, -1000), new Vector(0, 1, 0)) //
                 .setViewPlaneSize(30, 30).setDistance(350);//.setFocalPlane(100, 1, 1);
 
         scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
@@ -156,20 +156,236 @@ public class MP2_BVH {
 
 
         scene.geometries.add(
-                new Polygon(new Point3D(35,-10, -30), new Point3D(35, -40, 40),
-                        new Point3D(-40, -40, 40),  new Point3D(-40, -10, -30))
+                new Polygon(new Point3D(35,-20, -20), new Point3D(35, -40, 50),
+                        new Point3D(-40, -40, 50),  new Point3D(-40, -20, -20))
                         .setEmission(new Color(java.awt.Color.blue)) //floor
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
                 new Polygon(new Point3D(-28,-10, -30), new Point3D(-28, -15, -28),
                         new Point3D(-38, -15, -28),  new Point3D(-38, -10, -30))
-                        .setEmission(new Color(java.awt.Color.GREEN)) //cube
+                        .setEmission(new Color(java.awt.Color.red)) //cube1floor
                         .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+//                new Polygon(new Point3D(-28,-5, -30), new Point3D(-28, -10, -28),
+//                        new Point3D(-38, -10, -28),  new Point3D(-38, -5, -30))
+//                        .setEmission(new Color(java.awt.Color.gray)) //cube1up
+//                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-28,-5, -30), new Point3D(-28, -10, -30),
+                        new Point3D(-38, -15, -28),  new Point3D(-38, -10, -28))
+                        .setEmission(new Color(java.awt.Color.red)) //cube1-triangle up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-28,-5, -30), new Point3D(-28, -10, -30),
+                        new Point3D(-28, -15, -28),  new Point3D(-28, -10, -28))
+                        .setEmission(new Color(java.awt.Color.red)) //cube1right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-38, -15, -28),
+                        new Point3D(-28, -15, -28),  new Point3D(-28, -10, -28))
+                        .setEmission(new Color(java.awt.Color.red)) //cube1-triangle-front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-38, -10, -30),
+                        new Point3D(-28, -5, -30),  new Point3D(-28, -10, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube1-triangle-back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-25,-10, -30), new Point3D(-25, -15, -28),
+                        new Point3D(-15, -15, -28),  new Point3D(-15, -10, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 floor
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-25,-5, -30), new Point3D(-25, -10, -28),
+                        new Point3D(-15, -10, -28),  new Point3D(-15, -5, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-15,-5, -30), new Point3D(-15, -10, -28),
+                        new Point3D(-15, -15, -28),  new Point3D(-15, -10, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-25,-5, -30), new Point3D(-25, -10, -28),
+                        new Point3D(-25, -15, -28),  new Point3D(-25, -10, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-25,-10, -30), new Point3D(-25, -5, -30),
+                        new Point3D(-15, -5, -30),  new Point3D(-15, -10, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-25,-10, -28), new Point3D(-25, -15, -28),
+                        new Point3D(-15, -15, -28),  new Point3D(-15, -10, -28))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube2 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-8,-10, -30), new Point3D(-8, -15, -28),
+                        new Point3D(-13.5, -15, -28),  new Point3D(-13.5, -10, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 floor
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-8,-5, -30), new Point3D(-8, -10, -28),
+                        new Point3D(-13.5, -10, -28),  new Point3D(-13.5, -5, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-8,-10, -30), new Point3D(-8, -15, -28),
+                        new Point3D(-8, -10, -28),  new Point3D(-8, -5, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-13.5,-10, -30), new Point3D(-13.5, -15, -28),
+                        new Point3D(-13.5, -10, -28),  new Point3D(-13.5, -5, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-8,-10, -30), new Point3D(-8, -5, -30),
+                        new Point3D(-13.5, -5, -30),  new Point3D(-13.5, -10, -30))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-8,-10, -28), new Point3D(-8, -15, -28),
+                        new Point3D(-13.5, -15, -28),  new Point3D(-13.5, -10, -28))
+                        .setEmission(new Color(java.awt.Color.red)) //cube3 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(20, -10, -30), new Point3D(20, -15, -28),
+                        new Point3D(10, -15, -28),  new Point3D(10, -10, -30))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 floor
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(20, -5, -30), new Point3D(20, -10, -28),
+                        new Point3D(10, -10, -28),  new Point3D(10, -5, -30))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(20, -10, -30), new Point3D(20, -15, -28),
+                        new Point3D(20, -10, -28),  new Point3D(20, -5, -30))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(10, -10, -30), new Point3D(10, -15, -28),
+                        new Point3D(10, -10, -28),  new Point3D(10, -5, -30))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(10, -10, -30), new Point3D(20, -10, -30),
+                        new Point3D(20, -5, -30),  new Point3D(10, -5, -30))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(20, -15, -28), new Point3D(10, -15, -28),
+                        new Point3D(10, -10, -28),  new Point3D(20, -10, -28))
+                        .setEmission(new Color(java.awt.Color.blue)) //cube4 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(35, -10, -30), new Point3D(35, -15, -28),
+                        new Point3D(21, -15, -28),  new Point3D(21, -10, -30))
+                        .setEmission(new Color(java.awt.Color.gray)) //cube5 floor
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(35, -5, -30), new Point3D(35, -10, -28),
+                        new Point3D(21, -10, -28),  new Point3D(21, -5, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube5 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(35, -10, -30), new Point3D(35, -15, -28),
+                        new Point3D(35, -10, -28),  new Point3D(35, -5, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube5 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(21, -10, -30), new Point3D(21, -15, -28),
+                        new Point3D(21, -10, -28),  new Point3D(21, -5, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube5 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(21, -10, -30), new Point3D(35, -10, -30),
+                        new Point3D(35, -5, -30),  new Point3D(21, -5, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube5 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(35, -15, -28), new Point3D(21, -15, -28),
+                        new Point3D(21, -10, -28),  new Point3D(35, -10, -28))
+                        .setEmission(new Color(java.awt.Color.green)) //cube5 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(25, -2, -30), new Point3D(25, -8, -28),
+                        new Point3D(13, -8, -28),  new Point3D(13, -2, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 floor
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(25, 8, -30), new Point3D(25, 2, -28),
+                        new Point3D(13, 2, -28),  new Point3D(13, 8, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(25, -2, -30), new Point3D(25, 8, -30),
+                        new Point3D(25, 2, -28),  new Point3D(25, -8, -28))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(13, -2, -30), new Point3D(13, 8, -30),
+                        new Point3D(13, 2, -28),  new Point3D(13, -8, -28))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(25, -2, -30), new Point3D(25, 8, -30),
+                        new Point3D(13, 2, -30),  new Point3D(13, -8, -30))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(25, -2, -28), new Point3D(25, 8, -28),
+                        new Point3D(13, 2, -28),  new Point3D(13, -8, -28))
+                        .setEmission(new Color(java.awt.Color.orange)) //cube6 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-23,-2, -30), new Point3D(-23, -7, -28),
+                        new Point3D(-10, -7, -28),  new Point3D(-10, -2, -30))
+                        .setEmission(new Color(java.awt.Color.green)), //cube7 floor
+                     //   .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-23,0, -30), new Point3D(-23, -5, -28),
+                        new Point3D(-10, -5, -28),  new Point3D(-10, 0, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube7 up
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-10,-2, -30), new Point3D(-10, -7, -28),
+                        new Point3D(-10, -5, -28),  new Point3D(-10, 0, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube7 right
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-23,-2, -30), new Point3D(-23, -7, -28),
+                        new Point3D(-23, -5, -28),  new Point3D(-23, 0, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube7 left
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-23,-2, -30), new Point3D(-23, 0, -30),
+                        new Point3D(-10, 0, -30),  new Point3D(-10, -2, -30))
+                        .setEmission(new Color(java.awt.Color.green)) //cube7 back
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+                new Polygon(new Point3D(-23,-5, -28), new Point3D(-23, -7, -28),
+                        new Point3D(-10, -7, -28),  new Point3D(-10, -5, -28))
+                        .setEmission(new Color(java.awt.Color.green)) //cube7 front
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+
+
+
 //                new Polygon(new Point3D(35,-9, 30), new Point3D(35, -11, 28),
 //                        new Point3D(30, -11, 28),  new Point3D(30, -9, 30))
 //                        .setEmission(new Color(java.awt.Color.GREEN)) //floor
 //                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6))
                 new Sphere(1, new Point3D(0,-25,0))
-                .setEmission(new Color(java.awt.Color.GREEN))
+                .setEmission(new Color(java.awt.Color.GREEN)),
+
+                new Sphere(5, new Point3D(-25,-21,0))
+                        .setEmission(new Color(java.awt.Color.cyan))
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)),
+                new Sphere(3, new Point3D(-10,-28,0))
+                        .setEmission(new Color(java.awt.Color.magenta))
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6))
 //                ,new Polygon(new Point3D(0,0, 1500), new Point3D(55, -35, -500),
 //                        new Point3D(-30, -50, -1500), new Point3D(-30, 0, 1500))
 //                        .setEmission(new Color(java.awt.Color.GREEN)) //floor
